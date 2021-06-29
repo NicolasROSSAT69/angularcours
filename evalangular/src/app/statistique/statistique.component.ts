bf09e3ce2c8fc4b14df8c515960dd28bbc2a6883
nicolas <nicolas.rossat@hotmail.fr>
2021-06-29 11:33:28 +0200

    exercice 3

---
+++ evalangular/src/app/statistique/statistique.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Statistique } from '../models/statistique';
+@Component({
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.css']
+})
+export class StatistiqueComponent implements OnInit {
+
+  @Input()
+  public statistique!: Statistique;
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
